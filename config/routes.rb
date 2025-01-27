9ec6d23e5111b52f5dd2fab05a5d5d9e0af86ad3
平野達郎 <hiranotatsurou@hiranotatsurounoMacBook-Pro.local>
2025-01-27 11:28:07 +0900

    fix:サンプルのルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'sample#index'
 end
