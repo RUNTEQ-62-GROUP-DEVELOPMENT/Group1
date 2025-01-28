a97d29e78826322e919754ac7963ba4e139a5d4e
rana <nomumumu561@gmail.com>
2025-01-28 19:37:36 +0900

    modelファイル/migrationファイル作成

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :episodes
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  validates :title, presence: true
+  validates :meaning, presence: true
+end
