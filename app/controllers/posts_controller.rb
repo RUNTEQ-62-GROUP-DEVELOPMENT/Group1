f0f19177096d28029cfea052e1d1fd02be9f03f7
Hiruyan-J <hiruyan0428@gmail.com>
2025-02-16 17:29:59 +0900

    fix: バグ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def show
-    
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
