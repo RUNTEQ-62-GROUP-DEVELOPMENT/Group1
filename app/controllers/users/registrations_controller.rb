68a10870d6a9c7865e7e3e45702d313ee768c898
rina87 <hanchm0303@gmail.com>
2025-02-18 15:31:02 +0900

    02_ログイン機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    @user = User.new
-  end
+  # def new
+    # @user = User.new
+  # end
 
   # POST /resource
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end  
-  end
+  # def create
+   #  @user = User.new(configure_sign_up_params)
+    # if @user.save
+      # redirect_to root_path
+    # else
+      # render :new, status: :unprocessable_entity
+    # end  
+  # end
 
   # GET /resource/edit
   # def edit
@@ -42,17 +42,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  # protected
+  # def user_params
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  # end
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  # sign_up時のStrong Parameters
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -60,12 +61,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # 新規作成後の遷移先を指定
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+   root_path
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,38 +1,45 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-    def new; end
+    # def new; end
   
 
   # POST /resource/sign_in
-    def create
-      @user = login(params[:email], params[:password])
+    # def create
+      # @user = login(params[:email], params[:password])
 
-      if @user
-        redirect_to root_path
-      else
-        render :new, status: :unprocessable_entitye
-      end
-    end
+      # if @user
+        # redirect_to root_path
+      # else
+        # render :new, status: :unprocessable_entitye
+      # end
+    # end
 
   # DELETE /resource/sign_out
-  def destroy
-    logout
-    redirect_to root_path, status: :see_other
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # def destroy
+    # logout
+    # redirect_to root_path, status: :see_other
   # end
+
+  # private
+
+  # def user_params
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  # end
+  protected
+
+  def after_sign_in_path_for(resource) # ログイン後に遷移するpath指定
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope) # ログアウト後に遷移するpath指定
+    new_user_session_path
+  end
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
