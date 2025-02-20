54d98684059d599e97333efa61af2e43f2c5a4c6
Hiruyan-J <hiruyan0428@gmail.com>
2025-02-20 23:04:35 +0900

    fix: エピソード投稿時の画面読み込み不良の解消

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,11 +4,13 @@ class EpisodesController < ApplicationController
   def create
     binding.pry
     @episode = current_or_guest_user.episodes.build(episode_params)
-    # @episode = current_user.episodes.build(episode_params)
-    binding.pry
     if @episode.save
+      redirect_to post_path(episode_params[:post_id])
       # redirect_to  ワード詳細ページ＆成功フラッシュメッセージ「エピソードを投稿しました」
     else
+      @post = Post.find(episode_params[:post_id])
+      @episodes = @post.episodes.includes(:user).order(created_at: :desc)
+      render template: "posts/show", status: :unprocessable_entity
       # render: ワード詳細ページ＆失敗フラッシュメッセージ「エピソードを投稿できませんでした」
     end
   end
