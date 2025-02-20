d64f89a473b6ec9bd441bcf812471cbdb048ef62
Hiruyan-J <hiruyan0428@gmail.com>
2025-02-20 23:30:20 +0900

    update: GestUserの処理の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,23 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # def current_or_guest_user
+  #   current_user || gest_user = User.find(0) || User.create!(id: 0, name: "Gest", email: "gest@test.com", password: "gestuser")
+  # end
+  
   def current_or_guest_user
-    current_user || gest_user = User.find(0)
+    # current_user が存在すればそのまま返す
+    return current_user if current_user
+  
+    # ID 0 のユーザーを検索、見つからなければ作成する
+    guest_user = User.find_or_create_by(id: 0) do |user|
+      user.name = "Guest"
+      user.email = "guest@test.com"
+      user.password = "guestuser"
+    end
+  
+    # guest_user を返す
+    guest_user
   end
 
   protected
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :episodes
-    has_many :post_tags, dependent: :destroy
-    has_many :tags, through: :post_tags
   
     validates :title, presence: true, length: { maximum: 255 }
     validates :body, presence: true, length: { maximum: 65_535 }
