762195eb83476e8091bc1c25a48bdfd835f66599
Hiruyan-J <hiruyan0428@gmail.com>
2025-04-28 17:49:57 +0900

    update: ワード登録機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @posts = Post.includes(:user).order(created_at: :desc).page(params[:page])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
   end
 
   def show
